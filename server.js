a5e5f31eee57987cc9c89cc33ad0e43c0d225f0f
Sean Nelson <smnelsonn@gmail.com>
2019-06-13 21:16:54 -0700

    added standard initializing stuff/ test user post

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 require('dotenv').config(); 
 require('./lib/utils/connect')();
 const app = require('./lib/app');
-const archive = require('./lib/utils/archive');
-const powerCheck = require('./lib/utils/powerCheck');
-const ratingCheck = require('./lib/utils/ratingCheck');
+// const archive = require('./lib/utils/archive');
+// const powerCheck = require('./lib/utils/powerCheck');
+// const ratingCheck = require('./lib/utils/ratingCheck');
 
 const PORT = process.env.PORT || 8889;
 
 app.listen(PORT, () => {
-  setInterval(() => {
-    archive();
-    powerCheck();
-    ratingCheck();
-  }, (1000 * 60));
+  // setInterval(() => {
+  //   archive();
+  //   powerCheck();
+  //   ratingCheck();
+  // }, (1000 * 60));
   // eslint-disable-next-line no-console
   console.log(`LISTENING on ${PORT}`);
 });
